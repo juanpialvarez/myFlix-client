7db615d2f177164846ffdd3b07912d3c52ee86a9
juanpialvarez <juanpabloalvarezv@gmail.com>
2023-03-08 19:21:54 -0600

    undo merge for assignment

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,39 +1,15 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
-import { LoginView } from "../login-view/login-view";
-import { SignupView } from "../signup-view/signup-view";
 
 const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem("user"));
-  const storedToken = localStorage.getItem("token");
-  const [user, setUser] = useState(storedUser ? storedUser : null);
-  const [token, setToken] = useState(storedToken ? storedToken : null);
   const [movies, setMovies] = useState([]);
-  const [selectedMovie, setSelectedMovie] = useState(null);
-
   useEffect(() => {
-    if (!token) return;
-    fetch("https://myflix94.herokuapp.com/movies", {
-      headers: { Authorization: `Bearer ${token}` },
-    })
+    fetch("https://myflix94.herokuapp.com/movies")
       .then((response) => response.json())
       .then((data) => setMovies(data));
-  }, [token]);
-
-  if (!user) {
-    return (
-      <>
-        <LoginView
-          onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-          }}
-        />
-        <SignupView />
-      </>
-    );
-  }
+  });
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
     let similarMovies = movies.filter(
@@ -77,15 +53,6 @@ const MainView = () => {
           }}
         />
       ))}
-      <button
-        onClick={() => {
-          setUser(null);
-          setToken(null);
-          localStorage.clear();
-        }}
-      >
-        Logout
-      </button>
     </div>
   );
 };
